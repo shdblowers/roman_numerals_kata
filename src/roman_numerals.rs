b00e801dcf5a08495a97c33e313f3778b596b14d
Steven Blowers <shdblowers@gmail.com>
2017-10-14 22:50:09 +0100

    adding all possibilites to map function.

--- src/roman_numerals.rs
+++ src/roman_numerals.rs
@@ -1,8 +1,19 @@
 pub fn encode(mut input: u32) -> String {
 
     let mapping = [
-        ("V", 5),
-        ("I", 1)
+        ("M",  1000),
+        ("CM", 900),
+        ("D",  500),
+        ("CD", 400),
+        ("C",  100),
+        ("XC", 90),
+        ("L",  50),
+        ("XL", 40),
+        ("X",  10),
+        ("IX", 9),
+        ("V",  5),
+        ("IV", 4),
+        ("I",  1),
     ];
 
     let mut result = String::new();
@@ -12,6 +23,7 @@ pub fn encode(mut input: u32) -> String {
             if input >= decimal {
                 result = result + roman;
                 input = input - decimal;
+                break;
             }
         }
     }
@@ -34,7 +46,27 @@ mod tests {
     }
 
     #[test]
+    fn encode_4() {
+        assert_eq!("IV", encode(4));
+    }
+
+    #[test]
     fn encode_6() {
         assert_eq!("VI", encode(6));
+    }
+
+    #[test]
+    fn encode_1991() {
+        assert_eq!("MCMXCI", encode(1991));
+    }
+
+    #[test]
+    fn encode_4999() {
+        assert_eq!("MMMMCMXCIX", encode(4999));
+    }
+
+    #[test]
+    fn encode_444() {
+        assert_eq!("CDXLIV", encode(444));
     }
 }
